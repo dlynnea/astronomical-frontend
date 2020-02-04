eb092690e4a81af1dd45b8ce4966a16af28bd01f
Dani Matton <danielle@Danis-MacBook-Pro.local>
2020-02-04 11:07:00 -0700

    Initial app setup, sass implemented with basic styling

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>[logo/name]</h1>
+      </header>
+      <section className="landing">
+      <h2>Welcome to our [name]</h2>
+      <h3>Click here to enter</h3>
+      <button className="enter-card">Enter</button>
+      </section>
+    </div>
+  );
+}
+
+export default App;
